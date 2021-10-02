98685b71da934c547e61b29c084f4bf2334c62ba
swathi kp <61752414+swathi-kp-2298@users.noreply.github.com>
2021-10-03 00:21:23 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,54 @@
+// @bekbrace
+// FARMSTACK Tutorial - Sunday 13.06.2021
+ 
+import React, { useState, useEffect} from 'react';
+import './App.css';
+import TodoView from './components/TodoListView';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+
+function App() {
+
+  const [todoList, setTodoList] = useState([{}])
+  const [title, setTitle] = useState('') 
+  const [desc, setDesc] = useState('')
+  
+    
+
+  // Read all todos
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/todo')
+      .then(res => {
+        setTodoList(res.data)
+      })
+  });
+
+  // Post a todo
+  const addTodoHandler = () => {
+    axios.post('http://localhost:8000/api/todo/', { 'title': title, 'description': desc })
+      .then(res => console.log(res))
+};
+
+  return (
+    <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
+      <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Student details</h1>
+      <h6 className="card text-white bg-primary mb-3">	</h6>
+     <div className="card-body">
+      <h5 className="card text-white bg-dark mb-3">Add student name and qualification</h5>
+      <span className="card-text"> 
+        <input className="mb-2 form-control titleIn" onChange={event => setTitle(event.target.value)} placeholder='Name'/> 
+        <input className="mb-2 form-control desIn" onChange={event => setDesc(event.target.value)}   placeholder='Qualification'/>
+      <button className="btn btn-outline-primary mx-2 mb-3" style={{'borderRadius':'50px',"font-weight":"bold"}}  onClick={addTodoHandler}>Add Task</button>
+      </span>
+      <h5 className="card text-white bg-dark mb-3">view details</h5>
+      <div >
+      <TodoView todoList={todoList} />
+      </div>
+     </div>
+      
+    </div>
+  );
+}
+
+export default App;
